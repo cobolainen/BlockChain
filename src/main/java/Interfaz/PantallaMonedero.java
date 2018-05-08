259f8130052bd05aefd9793c86e7b30b8f89e771
cccob <cccob@DESKTOP-G24BS4E>
2018-05-08 22:51:58 +0200

    dgdf

--- src/main/java/Interfaz/PantallaMonedero.java
+++ src/main/java/Interfaz/PantallaMonedero.java
@@ -1,6 +1,7 @@
 package Interfaz;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.TextField;
 
@@ -19,13 +20,14 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import java.awt.Font;
 
 public class PantallaMonedero extends JFrame {
 	Monedero monedero;
 
 	private JPanel contentPane;
-	private TextField lblDireccion;
-	private JTable table;
 
 	/**
 	 * Create the frame.
@@ -36,56 +38,71 @@ public class PantallaMonedero extends JFrame {
 	public PantallaMonedero(Monedero mon) throws UnsupportedLookAndFeelException {
 		inicializar();
 		this.monedero = mon;
-		lblDireccion.setText(StringUtil.getStringDeclave(mon.clavePublica));
 		monedero.getBalance();
 	}
 
 	private void inicializar() throws UnsupportedLookAndFeelException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 508, 409);
+		setBounds(100, 100, 792, 608);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		JLabel lbllDireccion = new JLabel("Direccion:");
-		lbllDireccion.setBounds(50, 21, 93, 21);
-		contentPane.add(lbllDireccion);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 11, 756, 213);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblBalance = new JLabel("BALANCE");
+		lblBalance.setBounds(28, 58, 79, 14);
+		panel.add(lblBalance);
+		
+		JLabel lblNewLabel = new JLabel("CBC");
+		lblNewLabel.setBounds(28, 98, 138, 31);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("EQUIVALEN A:");
+		lblNewLabel_1.setBounds(226, 58, 98, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblEur = new JLabel("EUR");
+		lblEur.setBounds(226, 98, 138, 31);
+		panel.add(lblEur);
+		
+		JLabel lblAcciones = new JLabel("ACCIONES");
+		lblAcciones.setBounds(637, 58, 68, 14);
+		panel.add(lblAcciones);
+		
+		JButton btnNewButton = new JButton("ENVIAR");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBounds(494, 102, 89, 23);
+		btnNewButton.setBackground(new Color (9, 241, 202));
+		panel.add(btnNewButton);
+		
+		JButton btnRecibir = new JButton("RECARGAR");
+		btnRecibir.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnRecibir.setForeground(Color.WHITE);
+		btnRecibir.setBounds(607, 102, 98, 23);
+		btnRecibir.setBackground(new Color (9, 241, 202));
+		btnRecibir.setFocusPainted(false);
+		panel.add(btnRecibir);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(10, 224, 756, 334);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("ULTIMAS TRANSACCIONES");
+		lblNewLabel_2.setBounds(29, 25, 160, 24);
+		panel_1.add(lblNewLabel_2);
+		
+		JList list = new JList();
+		list.setBounds(29, 75, 671, 234);
+		panel_1.add(list);
 		UIManager.setLookAndFeel(new com.jtattoo.plaf.mcwin.McWinLookAndFeel());
-		lblDireccion = new TextField("New label");
-		lblDireccion.setEditable(false);
-		lblDireccion.setBounds(153, 22, 239, 19);
-		contentPane.add(lblDireccion);
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(50, 91, 388, 195);
-		contentPane.add(scrollPane);
-
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-			},
-			new String[] {
-				"Motivo", "Emisor", "Receptor", "Valor", "Fecha"
-			}
-		) {
-			boolean[] columnEditables = new boolean[] {
-				false, false, false, false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
-		table.getColumnModel().getColumn(0).setPreferredWidth(85);
-		table.getColumnModel().getColumn(1).setPreferredWidth(85);
-		table.getColumnModel().getColumn(2).setPreferredWidth(85);
-		table.getColumnModel().getColumn(3).setPreferredWidth(85);
-		table.getColumnModel().getColumn(4).setPreferredWidth(85);
 	}
 }
